b352a2e810c120c62a54216f2ca571ad1950ee78
svetlana <nooch@yandex.ru>
2022-10-19 22:18:01 +0300

    5.1

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿/*Задача 1: Задайте массив заполненный случайными 
+положительными трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+
+int[] NewArray(int len) //функция для вывода массива случайных 3хзн чисел
+{
+    int[] array = new int[len];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,999); 
+    }
+    return array;
+}
+
+void PrintArray(int[] array) //функция вывода на экран 
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		System.Console.Write($"{array[i]}\t");
+	}
+	System.Console.WriteLine();
+}
+
+int[] array = NewArray(5); 
+PrintArray(array);
+
+int CountEvenNumber(int number)// для нахождения количесва четных чисел в массиве
+{
+	int sum = 0;
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (array[i] % 2 == 0)
+		{
+			array[i] = 1;
+            sum = sum + array[i];
+		}
+	}
+	return sum;
+}
+
+int sum = 0;
+int even = CountEvenNumber(sum);
+Console.Write($"количество четных чисел в массиве равно {even}");
+
